928e2cb120bbc1713d894760f50bc5fc508e40e0
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-02 16:50:53 +0200

    5 primeras consultas

--- Exercicis_Tienda/exercicis_tienda.sql
+++ Exercicis_Tienda/exercicis_tienda.sql
@@ -2,3 +2,15 @@
 use tienda;
 
 SELECT nombre from tienda.producto;
+
+SELECT nombre, precio from tienda.producto;
+
+SELECT * FROM tienda.producto;
+
+SELECT nombre, precio, ROUND(precio * 1.08, 2) from tienda.producto;
+
+SELECT
+    nombre AS nombre_del_producto,
+    precio AS precio_en_euros,
+    ROUND(precio * 1.08, 2) AS precio_en_dolares
+from tienda.producto;
