858a9f6a8e9940be20282e1d79fbe0466997ab26
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-08 09:40:45 +0200

    separando las queries

--- Exercicis_Tienda/exercicis_tienda.sql
+++ Exercicis_Tienda/exercicis_tienda.sql
@@ -88,34 +88,54 @@ SELECT producto.nombre, producto.precio FROM tienda.producto INNER JOIN fabrican
 use universidad;
 
 SELECT apellido1, apellido2, nombre from universidad.persona where tipo = 'alumno' ORDER BY apellido1, apellido2, nombre;
+
 SELECT nombre, apellido1, apellido2 from universidad.persona where tipo = 'alumno' and telefono is null;
+
 SELECT nombre, apellido1, apellido2 from universidad.persona where tipo = 'alumno' and fecha_nacimiento between '1999-01-01' and '1999-12-31';
+
 SELECT nombre, apellido1, apellido2 from universidad.persona where tipo = 'profesor' and telefono is null and nif like '%K';
+
 SELECT nombre from universidad.asignatura where cuatrimestre = 1 and curso = 3 and id_grado = 7;
+
 SELECT p.apellido1, p.apellido2, p.nombre, d.nombre AS nombre_departamento FROM persona p INNER JOIN profesor pr ON p.id = pr.id_profesor INNER JOIN departamento d ON pr.id_departamento = d.id WHERE p.tipo = 'profesor' ORDER BY p.apellido1, p.apellido2, p.nombre;
 
 SELECT asignatura.nombre, curso_escolar.anyo_inicio, curso_escolar.anyo_fin from universidad.alumno_se_matricula_asignatura inner join asignatura on asignatura.id = id_asignatura inner join persona on persona.id = id_alumno inner join curso_escolar on curso_escolar.id = id_curso_escolar where persona.nif = '26902806M';
+
 SELECT DISTINCT d.nombre FROM departamento d INNER JOIN profesor p ON d.id = p.id_departamento INNER JOIN asignatura a ON p.id_profesor = a.id_profesor INNER JOIN grado g ON a.id_grado = g.id WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+
 SELECT DISTINCT nombre, apellido1, apellido2 from universidad.alumno_se_matricula_asignatura inner join persona on persona.id = id_alumno inner join curso_escolar on curso_escolar.id = id_curso_escolar where anyo_inicio = 2018 and anyo_fin = 2019;
 
-
 SELECT departamento.nombre, persona.apellido1, persona.apellido2, persona.nombre from universidad.profesor left join persona on persona.id = id_profesor left join departamento on id_departamento = departamento.id order by departamento.nombre, persona.apellido1, persona.apellido2, persona.nombre;
+
 select persona.nombre, persona.apellido1, persona.apellido2, departamento.nombre AS nombre_departamento from universidad.persona left join profesor on persona.id = id_profesor left join departamento on id_departamento = departamento.id where id_departamento is null order by persona.apellido1, persona.apellido2, persona.nombre;
+
 SELECT departamento.nombre from universidad.departamento left join profesor on id = id_profesor where id_profesor is null;
+
 SELECT persona.nombre, persona.apellido1, persona.apellido2 FROM universidad.persona LEFT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor WHERE profesor.id_profesor IS NULL;
+
 SELECT asignatura.nombre from universidad.asignatura left join persona on id_profesor = persona.id where id_profesor is null;
+
 SELECT departamento.nombre FROM universidad.departamento LEFT JOIN profesor ON departamento.id = profesor.id_departamento LEFT JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor WHERE profesor.id_profesor IS NULL;
 
 SELECT count(persona.id) from universidad.persona where persona.tipo = 'alumno';
 -- Calcula quants/es alumnes van néixer en 1999.
+
 SELECT count(persona.id) from universidad.persona where persona.tipo = 'alumno' and persona.fecha_nacimiento between '1999-01-01' and '1999-12-31';
+
 SELECT departamento.nombre, COUNT(*) as cantidad_profesores FROM universidad.departamento INNER JOIN profesor ON departamento.id = profesor.id_departamento GROUP BY departamento.nombre ORDER BY cantidad_profesores DESC;
+
 SELECT departamento.nombre, COUNT(*) as cantidad_profesores FROM universidad.departamento LEFT JOIN profesor ON departamento.id = profesor.id_departamento GROUP BY departamento.nombre;
+
 SELECT grado.nombre, COUNT(*) as cantidad_asignaturas FROM universidad.grado LEFT JOIN asignatura ON grado.id = asignatura.id_grado GROUP BY grado.nombre ORDER BY cantidad_asignaturas DESC;
+
 SELECT grado.nombre, COUNT(*) as cantidad_asignaturas FROM universidad.grado LEFT JOIN asignatura ON grado.id = asignatura.id_grado GROUP BY grado.nombre HAVING COUNT(*) > 40;
+
 SELECT grado.nombre, asignatura.tipo, SUM(asignatura.creditos) as total_creditos FROM universidad.grado INNER JOIN asignatura ON grado.id = asignatura.id_grado GROUP BY grado.nombre, asignatura.tipo;
+
 SELECT curso_escolar.anyo_inicio, COUNT(*) as cantidad_alumnos FROM universidad.alumno_se_matricula_asignatura INNER JOIN curso_escolar ON curso_escolar.id = id_curso_escolar GROUP BY anyo_inicio;
+
 SELECT persona.id, persona.nombre, persona.apellido1,  persona.apellido2, COUNT(asignatura.id) AS cantidad_asignaturas FROM universidad.persona LEFT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor GROUP BY persona.id ORDER BY cantidad_asignaturas DESC;
 
 SELECT * FROM universidad.persona WHERE tipo = 'alumno' ORDER BY fecha_nacimiento DESC LIMIT 1;
+
 SELECT persona.nombre, persona.apellido1, persona.apellido2 FROM universidad.persona LEFT JOIN profesor ON persona.id = profesor.id_profesor LEFT JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor WHERE asignatura.id_profesor IS NULL;
