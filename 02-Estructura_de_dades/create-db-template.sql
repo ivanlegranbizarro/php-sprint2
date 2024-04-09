320ba16839e10461520637fa499ccb78958bfcaf
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-09 11:31:38 +0200

    retoques optica

--- 02-Estructura_de_dades/create-db-template.sql
+++ 02-Estructura_de_dades/create-db-template.sql
@@ -22,22 +22,6 @@ CREATE TABLE empleat(
   nom VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE ulleres(
-id_ulleres INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-graduacio_esquerra FLOAT NOT NULL,
-graduacio_dreta FLOAT NOT NULL,
-tipus_muntura ENUM('flotant', 'pasta', 'metal·lica') NOT NULL,
-color_muntura VARCHAR(50) NOT NULL,
-color_vidre_esquerre VARCHAR(50) NOT NULL,
-color_vidre_dret VARCHAR(50) NOT NULL,
-preu FLOAT NOT NULL,
-id_proveidor_compra INT NOT NULL,
-FOREIGN KEY (id_proveidor_compra) REFERENCES proveidor(id_proveidor)
-id_venedor INT NOT NULL,
-FOREIGN KEY (id_venedor) REFERENCES empleat(id_empleat)
-);
-
-
 CREATE TABLE client(
 id_client INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(50) NOT NULL,
@@ -52,4 +36,22 @@ email VARCHAR(50) NOT NULL,
 data_registre TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 id_client_recomanador INT,
 FOREIGN KEY (id_client_recomanador) REFERENCES client(id_client)
+);
+
+
+CREATE TABLE ulleres(
+id_ulleres INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+graduacio_esquerra FLOAT NOT NULL,
+graduacio_dreta FLOAT NOT NULL,
+tipus_muntura ENUM('flotant', 'pasta', 'metal·lica') NOT NULL,
+color_muntura VARCHAR(50) NOT NULL,
+color_vidre_esquerre VARCHAR(50) NOT NULL,
+color_vidre_dret VARCHAR(50) NOT NULL,
+preu FLOAT NOT NULL,
+id_proveidor_compra INT NOT NULL,
+FOREIGN KEY (id_proveidor_compra) REFERENCES proveidor(id_proveidor)
+id_venedor INT NOT NULL,
+FOREIGN KEY (id_venedor) REFERENCES empleat(id_empleat)
+id_comprador INT NOT NULL,
+FOREIGN KEY (id_comprador) REFERENCES client(id_client)
 );
