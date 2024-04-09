9a393ad1f71d8b809419c27e749060af23436bf3
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-09 13:08:42 +0200

    tablas ultimadas

--- 02-Estructura_de_dades/create-db-template.sql
+++ 02-Estructura_de_dades/create-db-template.sql
@@ -3,18 +3,24 @@ DROP DATABASE IF EXISTS optica;
 CREATE DATABASE optica
     DEFAULT CHARACTER SET = 'utf8mb4';
 
+CREATE TABLE adreça(
+  id_adreça INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  carrer VARCHAR(50) NOT NULL,
+  numero INT NOT NULL,
+  pis VARCHAR(5) NOT NULL,
+  porta VARCHAR(5) NOT NULL,
+  ciutat VARCHAR(50) NOT NULL,
+  codi_postal VARCHAR(5) NOT NULL
+);
+
 CREATE TABLE proveidor(
 id_proveidor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(50) NOT NULL,
-carrer VARCHAR(50) NOT NULL,
-numero INT NOT NULL,
-pis CHAR(5) NOT NULL,
-porta CHAR(5) NOT NULL,
-ciutat VARCHAR(50) NOT NULL,
-codi_postal VARCHAR(5) NOT NULL,
 telefon VARCHAR(9) NOT NULL,
 fax VARCHAR(9) NOT NULL,
-NIF VARCHAR(9) NOT NULL
+NIF VARCHAR(9) NOT NULL,
+id_adreça_proveidor INT NOT NULL,
+FOREIGN KEY (id_adreça_proveidor) REFERENCES adreça(id_adreça)
 );
 
 CREATE TABLE empleat(
@@ -25,17 +31,13 @@ CREATE TABLE empleat(
 CREATE TABLE client(
 id_client INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(50) NOT NULL,
-carrer VARCHAR(50) NOT NULL,
-numero INT NOT NULL,
-pis VARCHAR(5) NOT NULL,
-porta VARCHAR(5) NOT NULL,
-ciutat VARCHAR(50) NOT NULL,
-codi_postal VARCHAR(5) NOT NULL,
 telefon VARCHAR(9) NOT NULL,
 email VARCHAR(50) NOT NULL,
 data_registre TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 id_client_recomanador INT,
-FOREIGN KEY (id_client_recomanador) REFERENCES client(id_client)
+FOREIGN KEY (id_client_recomanador) REFERENCES client(id_client),
+id_adreça_client INT NOT NULL,
+FOREIGN KEY (id_adreça_client) REFERENCES adreça(id_adreça)
 );
 
 CREATE TABLE ulleres(
@@ -47,11 +49,17 @@ color_muntura VARCHAR(50) NOT NULL,
 color_vidre_esquerre VARCHAR(50) NOT NULL,
 color_vidre_dret VARCHAR(50) NOT NULL,
 preu FLOAT NOT NULL,
-fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-id_proveidor_compra INT NOT NULL,
-FOREIGN KEY (id_proveidor_compra) REFERENCES proveidor(id_proveidor),
-id_venedor INT NOT NULL,
-FOREIGN KEY (id_venedor) REFERENCES empleat(id_empleat),
-id_comprador INT NOT NULL,
-FOREIGN KEY (id_comprador) REFERENCES client(id_client)
+id_ulleres_proveidor INT NOT NULL,
+FOREIGN KEY (id_ulleres_proveidor) REFERENCES proveidor(id_proveidor),
+);
+
+CREATE TABLE venta(
+id_venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+data_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+id_venta_client INT NOT NULL,
+FOREIGN KEY (id_venta_client) REFERENCES client(id_client),
+id_venta_ulleres INT NOT NULL,
+FOREIGN KEY (id_venta_ulleres) REFERENCES ulleres(id_ulleres),
+id_venta_empleat INT NOT NULL,
+FOREIGN KEY (id_venta_empleat) REFERENCES empleat(id_empleat)
 );
