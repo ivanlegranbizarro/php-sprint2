1051c8539c0e96db6edb0867ccbb5f837ce6c699
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-11 11:32:00 +0200

    kata numerology en php

---
+++ katas/kataNumerology.php
@@ -0,0 +1,41 @@
+<?php
+
+function convertir_num_a_array($num)
+{
+  $array = array_map('intval', str_split($num));
+  return $array;
+}
+
+function sumar_array($array)
+{
+  $calculo = array_sum($array);
+  if ($calculo > 9) {
+    return intval(strval($calculo)[0]);
+  }
+  return $calculo;
+}
+
+function procesar_numero($num)
+{
+  $array_numero = convertir_num_a_array($num);
+  $reduccion = sumar_array($array_numero);
+
+  return match ($reduccion) {
+    0 => "Tot",
+    1 => "Individualitat",
+    2 => "Dualitat",
+    3 => "Raó",
+    4 => "Estabilitat",
+    5 => "Amor",
+    6 => "Destrucció",
+    7 => "Saviessa",
+    8 => "Poder",
+    9 => "Generositat",
+    default => "Error",
+  };
+}
+
+$array_numero_1 = [1, 3];
+$array_numero_2 = [1, 7, 2];
+
+echo procesar_numero(172);
